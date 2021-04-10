c2d952f091408aebcfa8ca5037957842a10e5f00
Brian Daley <brian.daley@uconn.edu>
2021-04-09 23:32:01 -0400

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main id="main">
+      <v-btn color="primary" @click="openCamera"><v-icon>mdi-plus</v-icon> Add Scan</v-btn>
+      <v-bottom-sheet 
+      v-model="camera"
+      overlay-color="black"
+      fullscreen 
+      class="d-flex flex-column"
+      >
+
+        <div class="vc-container d-flex flex-column justify-end align-center">
+          <video class="flex-grow-1" ref="video" autoplay></video>
+          <div class="buttons">
+            <v-btn  
+              @click="camera=!camera"
+              fab
+              elevation="0"
+              small
+            ><v-icon>mdi-camera-switch-outline</v-icon></v-btn>
+
+            <v-btn  
+              fab
+              color="primary"
+              large
+              elevation="0"
+
+            ><v-icon>mdi-camera</v-icon></v-btn>
+
+                        <v-btn  
+              @click="camera=!camera"
+              fab
+              elevation="0"
+              small
+            ><v-icon>mdi-close</v-icon></v-btn>
+
+          </div>
+        </div>
+
+        <!--  -->
+
+
+        
+      </v-bottom-sheet>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data: () => ({
+    camera: false,
+    imageCapture: null
+  }),
+  methods: {
+    openCamera(){
+      navigator.mediaDevices.getUserMedia({video: true}).then(mediaStream => {
+
+        this.$refs.video.srcObject = mediaStream
+
+        const track = mediaStream.getVideoTracks()[0];
+        this.imageCapture = new ImageCapture(track);
+      })
+      .catch(error => console.log(error));   
+      this.camera=true   
+    },
+    closeCamera(){
+
+    }
+
+  },
+  mounted() {
+
+
+
   }
-}
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-main__wrap{
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+.vc-container {
+  height: 100vh;
+}
+
+.vc-container .buttons {
+  width: 100%;
+  background-color: black;
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.vc-container video {
+  background-color: black;
+}
+
 </style>
